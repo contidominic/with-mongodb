080ffb15f63c3b7d3bc193c7282b349f7af9fd95
contidominic <74687438+contidominic@users.noreply.github.com>
2021-04-25 21:59:37 +0100

    Update mongodb.js

--- util/mongodb.js
+++ util/mongodb.js
@@ -1,7 +1,6 @@
 import { MongoClient } from 'mongodb'
 
-const { MONGODB_URI, MONGODB_DB } = process.env
-
+const { MONGODB_URI, MONGODB_DB } = process.env.API_URL
 if (!MONGODB_URI) {
   throw new Error(
     'Please define the MONGODB_URI environment variable inside .env.local'
